702bc0f2d8212dd4fd2e15c853d45f22a0d52868
RuSaG0 <mirzoev-ruslan-2000@mail.ru>
2025-07-21 00:58:16 +0300

    feat: если нет песен - перестать проигрывать

--- src/structures/list/examples/music-player/music-player.vue
+++ src/structures/list/examples/music-player/music-player.vue
@@ -41,22 +41,27 @@ const updateProgress = () => {
 }
 
 const removeCurrentSong = () => {
-  if (!currentSong.value) return
+  if (!currentSong.value) return;
 
-  const currentSongTitle = currentSong.value.value.title
-  playlist.value.removeCurrent()
-  songs.value = songs.value.filter((song) => song.title !== currentSongTitle)
+  const currentSongTitle = currentSong.value.value.title;
+  playlist.value.removeCurrent();
+  songs.value = songs.value.filter((song) => song.title !== currentSongTitle);
 
   if (playlist.value.current) {
-    currentSong.value = playlist.value.current
+    currentSong.value = playlist.value.current;
     if (audioPlayer.value) {
-      audioPlayer.value.src = currentSong.value.value.src
+      audioPlayer.value.src = currentSong.value.value.src;
       if (isPlaying.value) {
-        audioPlayer.value.play()
+        audioPlayer.value.play();
       }
     }
   } else {
-    currentSong.value = null
+    currentSong.value = null;
+    if (audioPlayer.value) {
+      audioPlayer.value.pause();
+      audioPlayer.value.src = '';
+      isPlaying.value = false;
+    }
   }
 }
 
